68fdb741f96834dc5e12e2d7c25cb9cb5d0ec0a6
b0ndue11e <kazzantipoff@gmail.com>
2018-07-25 11:38:47 +0300

    redacted some templates

--- mods/models.py
+++ mods/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
+
 class Mod(models.Model):
     name = models.CharField(max_length=200, unique=True, help_text='Название модификатора')
-    short_name = models.CharField(max_length=10, unique=True,help_text='Короткое описание')
+    short_name = models.CharField(max_length=10, unique=True, help_text='Короткое описание')
     descriprion = models.TextField(help_text='Полное, или доступное, описания предмета или его действия')
     common = models.CharField(max_length=5, blank=True, help_text='Степень воздействия если предмет очень частый ')
     rare = models.CharField(max_length=50, blank=True, help_text='Степень воздействия если предмет редкий')
     very_rare = models.CharField(max_length=50, blank=True, help_text='Степень воздействия если предмет очень редкий')
     image = models.ImageField(upload_to='mods/')
+
 
 class Instruction(models.Model):
     name = models.CharField(max_length=255)
